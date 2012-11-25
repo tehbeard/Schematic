2d34fb7bceb628933152617a4e444cc76bbe702e
James <beardyjames@live.co.uk>
2012-11-25 02:27:23 +0000

    add retention to APIWorker

--- src/main/java/com/tehbeard/map/factory/APIWorker.java
+++ src/main/java/com/tehbeard/map/factory/APIWorker.java
@@ -1,5 +1,9 @@
 package com.tehbeard.map.factory;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface APIWorker {
 API value();
     public enum API{
