61933142d1c15fd458f6ebe86c579bbb9678ee3f
James <beardyjames@live.co.uk>
2013-03-09 22:03:37 +0000

    Finish spawnerData

--- src/main/java/com/tehbeard/map/misc/spawner/SpawnerData.java
+++ src/main/java/com/tehbeard/map/misc/spawner/SpawnerData.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.tehbeard.map.entities.Entity;
+import com.tehbeard.map.factory.WorkerFactory;
 import com.tehbeard.mojang.nbt.CompoundTag;
 import com.tehbeard.mojang.nbt.ListTag;
 
@@ -31,5 +32,64 @@ public class SpawnerData {
 		for( CompoundTag spTag : (ListTag<CompoundTag>) tag.getList("SpawnPotentials")){
 			spawnPotentials.add(new SpawnPotential(spTag));
 		}
+		
+		entityId = tag.getString("EntityId");
+		spawnData = WorkerFactory.getInstance().getEntity(entityId,tag.getCompound("SpawnData"));
+		
+		spawnCount = tag.getShort("spawnCount");
+		spawnRange = tag.getShort("spawnRange");
+		delay = tag.getShort("delay");
+		minSpawnDelay = tag.getShort("minSpawnDelay");
+		maxSpawnDelay = tag.getShort("maxSpawnDelay");
+		
+		maxNearbyEntities = tag.getShort("maxNearbyEntities");
+		requiredPlayerRange = tag.getShort("requiredPlayerRange");
+		
+		
 	}
+
+	public List<SpawnPotential> getSpawnPotentials() {
+		return spawnPotentials;
+	}
+
+	@Deprecated
+	public String getEntityId() {
+		return entityId;
+	}
+
+	@Deprecated
+	public Entity getSpawnData() {
+		return spawnData;
+	}
+
+	public short getSpawnCount() {
+		return spawnCount;
+	}
+
+	public short getSpawnRange() {
+		return spawnRange;
+	}
+
+	public short getDelay() {
+		return delay;
+	}
+
+	public short getMinSpawnDelay() {
+		return minSpawnDelay;
+	}
+
+	public short getMaxSpawnDelay() {
+		return maxSpawnDelay;
+	}
+
+	public short getMaxNearbyEntities() {
+		return maxNearbyEntities;
+	}
+
+	public short getRequiredPlayerRange() {
+		return requiredPlayerRange;
+	}
+	
+	
+	
 }
